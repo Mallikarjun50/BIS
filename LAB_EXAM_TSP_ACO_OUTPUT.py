9e31ccbcb91c4a956febfa3c44addfecaa861609
Mallikarjun50 <mallikarjun.cs23@bmsce.ac.in>
2025-11-10 14:01:58 +0530

    Create LAB_EXAM_TSP_ACO_OUTPUT.py

---
+++ LAB_EXAM_TSP_ACO_OUTPUT.py
@@ -0,0 +1,28 @@
+Iteration 01/25 | Best cost so far: 68.30
+Iteration 02/25 | Best cost so far: 68.30
+Iteration 03/25 | Best cost so far: 68.30
+Iteration 04/25 | Best cost so far: 68.30
+Iteration 05/25 | Best cost so far: 68.30
+Iteration 06/25 | Best cost so far: 68.30
+Iteration 07/25 | Best cost so far: 68.30
+Iteration 08/25 | Best cost so far: 68.30
+Iteration 09/25 | Best cost so far: 68.30
+Iteration 10/25 | Best cost so far: 68.30
+Iteration 11/25 | Best cost so far: 68.30
+Iteration 12/25 | Best cost so far: 68.30
+Iteration 13/25 | Best cost so far: 68.30
+Iteration 14/25 | Best cost so far: 68.30
+Iteration 15/25 | Best cost so far: 68.30
+Iteration 16/25 | Best cost so far: 68.30
+Iteration 17/25 | Best cost so far: 68.30
+Iteration 18/25 | Best cost so far: 68.30
+Iteration 19/25 | Best cost so far: 68.30
+Iteration 20/25 | Best cost so far: 68.30
+Iteration 21/25 | Best cost so far: 68.30
+Iteration 22/25 | Best cost so far: 68.30
+Iteration 23/25 | Best cost so far: 68.30
+Iteration 24/25 | Best cost so far: 68.30
+Iteration 25/25 | Best cost so far: 68.30
+
+✅ Optimal Route: [0, 1, 2, 3, 4, 0]
+💰 Total Cost (Distance + Penalty): 68.30
